58ac183e989b8643069c239776018625b53ed8f0
Ikey Doherty <ikey@serpentos.com>
2023-08-27 08:02:36 +0100

    Add initial dub scaffolding to build a library
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/stone/package.d
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+/**
+ * stone
+ *
+ * Main `.stone` library
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: MPL-2.0
+ */
+
+module stone;
