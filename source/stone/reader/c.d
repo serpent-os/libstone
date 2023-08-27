92bb6f3bc41d3003d0555e8b969b810793fe4a1d
Ikey Doherty <ikey@serpentos.com>
2023-08-27 08:31:55 +0100

    reader: Add new reserved `c` module for external API
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/stone/reader/c.d
@@ -0,0 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+/**
+ * stone.reader.c
+ *
+ * External C API to access stone.reader APIs outside D
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: MPL-2.0
+ */
+
+module stone.reader.c;
+
+@safe:
+
+extern(C)
+{
+    export:
+}
