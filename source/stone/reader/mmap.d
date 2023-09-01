0be51f7f838e4238577e8547d1cae6d2f2bc5767
Ikey Doherty <ikey@serpentos.com>
2023-09-01 23:18:55 +0100

    mmap: Make destructor accessible
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/stone/reader/mmap.d
+++ source/stone/reader/mmap.d
@@ -59,11 +59,22 @@ public struct MappedFile
         return () @trusted { return cast(ubyte[])(dataPage[start .. end]); }();
     }
 
+    ubyte[] opSlice() => opSlice[0 .. fileSize];
+
     /** 
      * Returns: Length of the mapped file
      */
     auto opDollar() @nogc nothrow => fileSize;
     alias length = opDollar;
+
+    ~this() @trusted nothrow @nogc
+    {
+        if (fd <= 0)
+            return;
+        dataPage.munmap(fileSize);
+        fd.close;
+        fd = 0;
+    }
 
 private:
 
@@ -85,15 +96,6 @@ private:
         dataPage = mmap(null, result.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
         if (dataPage is null)
             return;
-    }
-
-    ~this() @trusted nothrow @nogc
-    {
-        if (fd <= 0)
-            return;
-        dataPage.munmap(fileSize);
-        fd.close;
-        fd = 0;
     }
 
     /* Underlying file descriptor */
